0f55822bc93dda5712f1067073f2bbb0d2155559
Jonathan <jxb6065@cs.rit.edu>
2011-06-10 16:45:26 -0400

    pulled 'magic values' out into constants. minor cleanup of code formatting. added some TODO items to the code

--- jquery.iepngfix.js
+++ jquery.iepngfix.js
@@ -1,20 +1,32 @@
-// TODO: minify this
+// TODO: write a script to auto-generate a minified
+//       version of this file
 (function ($) {
 if (!$) return;
+
+var DEFAULT_EMPTY_IMAGE_PATH = "empty.gif";
+var DEFAULT_SIZING_METHOD = "scale";
+
 $.fn.extend({
-	fixPNG: function(sizingMethod, forceBG) {
+	// TODO: document the purpose of each of these parameters in high detail
+	fixPNG: function(sizingMethod, forceBG, emptyImagePath) {
+		// Don't bother with non-IE browsers
 		if (!($.browser.msie)) return this;
-		var emptyimg = "empty.gif"; //Path to empty 1x1px GIF goes here
-		sizingMethod = sizingMethod || "scale"; //sizingMethod, defaults to scale (matches image dimensions)
+		
+		// Path to empty 1x1px GIF goes here
+		emptyImagePath = emptyImagePath || DEFAULT_EMPTY_IMAGE_PATH;
+		
+		// Sizing method defaults to scale (matches image dimensions)
+		sizingMethod = sizingMethod || DEFAULT_SIZING_METHOD;
+		
 		this.each(function() {
 			var isImg = (forceBG) ? false : jQuery.nodeName(this, "img");
 			var imgname = (isImg) ? this.src : this.currentStyle.backgroundImage;
 			var src = (isImg) ? imgname : imgname.substring(5,imgname.length-2);
 			this.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + src + "', sizingMethod='" + sizingMethod + "')";
 			if (isImg) {
-				this.src = emptyimg;
+				this.src = emptyImagePath;
 			} else {
-				this.style.backgroundImage = "url(" + emptyimg + ")";
+				this.style.backgroundImage = "url(" + emptyImagePath + ")";
 			}
 		});
 		
