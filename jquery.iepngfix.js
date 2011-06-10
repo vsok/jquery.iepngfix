5c589acb83fbf19bd203e31f5fd20cfb08c73385
Jonathan <jxb6065@cs.rit.edu>
2011-06-10 16:40:03 -0400

    cleaned up source formatting

--- jquery.iepngfix.js
+++ jquery.iepngfix.js
@@ -2,19 +2,23 @@
 (function ($) {
 if (!$) return;
 $.fn.extend({
-    fixPNG: function(sizingMethod, forceBG) {
-            if (!($.browser.msie)) return this;
-            var emptyimg = "empty.gif"; //Path to empty 1x1px GIF goes here
-            sizingMethod = sizingMethod || "scale"; //sizingMethod, defaults to scale (matches image dimensions)
-            this.each(function() {
-                    var isImg = (forceBG) ? false : jQuery.nodeName(this, "img"),
-                            imgname = (isImg) ? this.src : this.currentStyle.backgroundImage,
-                            src = (isImg) ? imgname : imgname.substring(5,imgname.length-2);
-                    this.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + src + "', sizingMethod='" + sizingMethod + "')";
-                    if (isImg) this.src = emptyimg;
-                    else this.style.backgroundImage = "url(" + emptyimg + ")";
-            });
-            return this;
-    }
+	fixPNG: function(sizingMethod, forceBG) {
+		if (!($.browser.msie)) return this;
+		var emptyimg = "empty.gif"; //Path to empty 1x1px GIF goes here
+		sizingMethod = sizingMethod || "scale"; //sizingMethod, defaults to scale (matches image dimensions)
+		this.each(function() {
+			var isImg = (forceBG) ? false : jQuery.nodeName(this, "img");
+			var imgname = (isImg) ? this.src : this.currentStyle.backgroundImage;
+			var src = (isImg) ? imgname : imgname.substring(5,imgname.length-2);
+			this.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + src + "', sizingMethod='" + sizingMethod + "')";
+			if (isImg) {
+				this.src = emptyimg;
+			} else {
+				this.style.backgroundImage = "url(" + emptyimg + ")";
+			}
+		});
+		
+		return this;
+	}
 });
 })(jQuery);
