279b055cd944de373a37c5654a6301511258bbfe
Jonathan <jxb6065@cs.rit.edu>
2011-06-14 13:07:11 -0400

    pulled code out of the .each loop that didn't need to be in there. removed an unecessary inner .each loop from before I refactored in the chainability

--- jquery.iepngfix.js
+++ jquery.iepngfix.js
@@ -24,36 +24,34 @@
 	// sizingMethod: either "crop" or "scale"
 	jquery.fn.fixPNG = function (sizingMethod, forceBG) {
 		
+		// Don't bother with non-IE browsers
+		if (!($.browser.msie)) return $(this);
+		
+		// Sizing method defaults to scale (matches image dimensions)
+		// TODO: enforce either "crop" or "scale"
+		sizingMethod = sizingMethod || DEFAULT_SIZING_METHOD;
+		
 		return this.each(function() {
 			
 			var $this = $(this);
-			
-			// Don't bother with non-IE browsers
-			if (!($.browser.msie)) return $this;
 
-			// Sizing method defaults to scale (matches image dimensions)
-			// TODO: enforce either "crop" or "scale"
-			sizingMethod = sizingMethod || DEFAULT_SIZING_METHOD;
+			// determine if this node is an image
+			var isImg = forceBG ? false : jQuery.nodeName($this, "img");
 
-			$this.each(function() {
-				// determine if this node is an image
-				var isImg = forceBG ? false : jQuery.nodeName($this, "img");
+			// image name
+			var imgName = isImg ? $this.src : $this.currentStyle.backgroundImage;
 
-				// image name
-				var imgName = isImg ? $this.src : $this.currentStyle.backgroundImage;
+			// image src
+			var src = isImg ? imgName : imgName.substring(5,imgName.length-2);
 
-				// image src
-				var src = isImg ? imgName : imgName.substring(5,imgName.length-2);
+			// Set the "AlphaImageLoader" proprietary filter IE filter
+			$this.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + src + "', sizingMethod='" + sizingMethod + "')";
 
-				// Set the "AlphaImageLoader" proprietary filter IE filter
-				$this.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + src + "', sizingMethod='" + sizingMethod + "')";
-
-				if (isImg) {
-					$this.src = SHIM_IMAGE;
-				} else {
-					$this.style.backgroundImage = "url(" + SHIM_IMAGE + ")";
-				}
-			});
+			if (isImg) {
+				$this.src = SHIM_IMAGE;
+			} else {
+				$this.style.backgroundImage = "url(" + SHIM_IMAGE + ")";
+			}
 
 			return $this;
 		}
